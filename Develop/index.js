f3a743f26ebb32e67c171e84756558651d0c033a
Cyberdev <turnerjeniah8@gmail.com>
2022-11-16 22:56:19 -0800

    Fixed the terminal to ask all questions. Need to fix the createfile function in order for a README to be generated. Still recieving a error in the terminal for the createfile function

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const fs = require("fs");
+const fs = require('fs');
 const { writeFile } = require("../Develop/utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
@@ -129,12 +129,23 @@ const promptUser = () => {
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) throw err;
+        console.log ('Success');
+    });
+};
 
+//function to initialize program
+async function init() {
+    const answers = await inquirer.prompt;
+    const markdown = generateMarkdown(answers);
+    writeToFile('../Develop/utils/generateMarkdown', markdown);
+  }
 // TODO: Create a function to initialize app
 function init() {
     promptUser().then((data) => {
-        writeFile(data);
+        writeToFile(data);
     });
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# readmeGenerator
+# ReadMe Generator
+# Screenshot 
+
+# Description
+The goal of this project is to successfully create a README generator that will intake user information and format it into a detailed README file. 
+
+# Motivation
+The motivation for this project was to successfully troubleshoot and become more comfortable with working with node. I also wanted to successfully make a easier/faster way to create a README file
+
+# Language Used
+* Javascript
+
